d6488b0e4b934a7269e623fc1b58619f311f4a01
Jesús Bossa <me@jesusbossa.com>
2018-10-10 23:23:23 -0500

    Subjects users improved

--- app/controllers/api/v1/subject_users_controller.rb
+++ app/controllers/api/v1/subject_users_controller.rb
@@ -8,7 +8,7 @@ module Api
             end
 
             def show
-                enrollment = SubjectUser.find(params[:id])
+                enrollment = SubjectUser.where(:user_id params[:id]).first
                 render json: {status: 200, data:enrollment}, status: :ok
             end
 
@@ -23,7 +23,7 @@ module Api
             end
 
             def update
-                enrollment = SubjectUser.find(params[:id])
+                enrollment = SubjectUser.where(:user_id params[:id]).first
                 if enrollment.update_attributes(ref_params)
                     render json: {status: 200, data:enrollment}, status: :updated
                 else 
@@ -32,7 +32,7 @@ module Api
             end
 
             def destroy
-                enrollment = SubjectUser.find(params[:id])
+                enrollment = SubjectUser.where(:user_id params[:id]).first
                 enrollment.destroy
                 render json: {status: 200, data:enrollment}, status: :deleted
             end
