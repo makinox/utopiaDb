1403320d8f6d59ef3f42e2f29c105dc1cce83aa9
Jesús Bossa <me@jesusbossa.com>
2018-11-04 20:40:15 -0500

    [Improvement] Relation subject users erase

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,4 @@
 class Subject < ApplicationRecord
     validates :name, presence: true
     validates :grade, presence: true
-    has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   belongs_to :entity
-  has_and_belongs_to_many :subjects
   validates :name, presence: true
   validates :user, presence: true
   validates :email, presence: true
